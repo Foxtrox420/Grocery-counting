116e81d28c9f2b517fad91e48ba4c46d9fd9f86f
Ignatius Kennard <79687001+Foxtrox420@users.noreply.github.com>
2022-09-28 17:05:43 +0700

    Add files via upload

---
+++ pseudo code.c
@@ -0,0 +1,31 @@
+//Ignatius Kennard's pseudo code, please don't run it :) just using this for notepad
+
+//in this section, the integers that will be needed are declared in this section
+Declare Integer eggprice
+Declare Integer meatprice
+Declare Integer milkprice
+Declare Integer biscuitprice
+Declare Integer eggamount
+Declare Integer meatamount
+Declare Integer milkprice
+Declare Integer biscuit price
+Declare Real totalprice
+
+//the integers : eggprice, meatprice, milkprice, biscuit price 
+//these integers are given a set value in this section which would be used later on in the calculation process
+set eggprice = 2.00
+set meatprice = 10.00
+set milkprice = 4.00
+set biscuitprice = 5.00 
+
+//in here, we don't need to input the price of the respective groceries as the values are already set before, -
+//- therefore the buyer only needs to give the amount. This reduces the need to input price over and over again. 
+Display "Please insert the amount of desired groceries"
+input eggamount
+input meatamount
+input milkamount
+input biscuitamount
+set totalprice=(eggprice*eggamount)+(meatprice*meatamount)+(milkprice*milkamount)+(biscuitprice*biscuit*amount)
+
+//in this last phase, the total price of the desired items of the customer is displayed and succesfully calculated. 
+>
