8fbc9048bd2637b468c28587e7b9119bd9374d06
Ignatius Kennard <79687001+Foxtrox420@users.noreply.github.com>
2022-10-03 10:43:01 +0700

    Update
    shorten the code to create efficiency

--- Groceries Project Remastered.cpp
+++ Groceries Project Remastered.cpp
@@ -8,14 +8,9 @@ int main(){
 	int biscuitprice = 5;
 	int eggamount, meatamount, milkamount, biscuitamount;
 	
-	printf("Insert the amount of eggs you want : ");
-	scanf("%d", &eggamount );
-	printf("Insert the amount of meat you want : ");
-	scanf("%d", &meatamount);
-	printf("Insert the amount of milk you want : ");
-	scanf("%d", &milkamount);
-	printf("Insert the amount of biscuit you want: ");
-	scanf("%d", &biscuitamount);
-	printf("The price will be a grand total of : ");
-	printf("%d\n", eggamount*eggprice+meatamount*meatprice+milkamount*milkprice+biscuitamount*biscuitprice);
+	printf("Insert the amount of eggs you want : "); scanf("%d", &eggamount );
+	printf("Insert the amount of meat you want : "); scanf("%d", &meatamount);
+	printf("Insert the amount of milk you want : "); scanf("%d", &milkamount);
+	printf("Insert the amount of biscuit you want: "); scanf("%d", &biscuitamount);
+	printf("The price will be a grand total of : "); printf("%d\n", eggamount*eggprice+meatamount*meatprice+milkamount*milkprice+biscuitamount*biscuitprice);
 }
